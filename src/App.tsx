384080b509da214a6e6f1eab3a46c7414ba1d414
Rylan Zhou <rylanzhou@outlook.com>
2023-02-21 18:15:59 -0600

    core: load project package, build basic structure

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+import Layout from './Layout';
+import './styles/App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <Layout>Something</Layout>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/assets/index.ts
@@ -0,0 +1,3 @@
+export { ReactComponent as Close } from './icon-close.svg';
+export { ReactComponent as Hamburger } from './icon-hamburger.svg';
+export { ReactComponent as Logo } from './logo.svg';
